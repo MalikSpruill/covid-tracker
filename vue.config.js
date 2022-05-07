56c2bec80e61c338a260ce50999a1b01baad6c90
Malik <malikspruill@gmail.com>
2022-05-06 23:05:30 -0400

    Added additional sub-directories and files.  Previous commits altered on previous error.  Covid-tracker directory recreated

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+//https://cli.vuejs.org/guide/deployment.html#github-pages
